a0a84694c1437c8387bdb3ab96ed0fe53f90dac0
Tobias Kamenicky <tobias.kamenicky@instech.no>
2021-12-08 15:57:37 +0100

    Improve day 1

--- day-1/solution1.jl
+++ day-1/solution1.jl
@@ -1,8 +1,8 @@
 input = open(f->read(f, String), "input1.txt")
-numbers = [parse(Int, n) for n in split(input, "\n"; keepempty=false)]
+numbers = parse.(Int, split(input, "\n"; keepempty=false))
 
 # Part 1
-println(sum([numbers[i-1] < numbers[i] for i=2:lastindex(numbers)]))
+println(count(diff(numbers) .> 0))
 
 # Part 2
 println(sum([numbers[i-3] < numbers[i] for i=4:lastindex(numbers)]))
